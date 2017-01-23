122a79d9ce12c63bb4bf76b67ffefa1906b464d8
Jose Maria Sobrinos <libreyextremo@gmail.com>
2017-01-23 17:06:29 +0100

    Add files via upload
    new examples

---
+++ example_10/controller.js
@@ -0,0 +1,17 @@
+﻿angular
+.module('acumuladorApp', [])
+.controller("acumuladorAppCtrl", controladorPrincipal);
+
+function controladorPrincipal(){
+	var scope = this;
+	scope.total = 0;
+	scope.cuanto = 0;
+
+	scope.sumar = function() {
+		scope.total += parseInt(scope.cuanto);
+	}
+
+	scope.restar = function() {
+		scope.total -= parseInt(scope.cuanto);
+	}
+};
---
+++ example_11/controller.js
@@ -0,0 +1,15 @@
+﻿var acumuladorApp= angular.module('acumuladorApp', [])
+acumuladorApp.controller("acumuladorAppCtrl", 
+	["$scope", function($scope) {
+		$scope.total = 0;
+		$scope.cuanto = 0;
+
+		$scope.sumar = function() {
+			$scope.total += parseInt($scope.cuanto);
+		}
+
+		$scope.restar = function() {
+			$scope.total -= parseInt($scope.cuanto);
+		}
+
+	}]);
